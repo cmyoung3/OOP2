322e80c210b981255fa96809fbf8d715fd2843f5
cmyoung3 <Caleb_Young1@baylor.edu>
2021-09-15 16:05:19 -0500

    Initializing on my laptop

---
+++ InsectFlight.py
@@ -0,0 +1,11 @@
+import InsectClass as i
+
+
+mosquito = i.Insect()
+housefly = i.Insect()
+
+mosquito.flightLen()
+
+housefly.flightLen()
+print("the mosquito can fly up to", mosquito.get_miles(), "miles")
+print("the housefly can fly up to", housefly.get_miles(), "miles")
